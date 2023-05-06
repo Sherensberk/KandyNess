86f8c5773d274a12be936f2967570537ac35eedd
Joao Vitor Siqueira <jvitorsiqueira99@gmail.com>
2023-05-06 15:23:55 -0300

    Layout pagina da loja,componentes productCard e Avatar

---
+++ kandyness/src/components/pcard.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-card class="mx-auto my-12" max-width="250">
+    <template slot="progress">
+      <v-progress-linear
+        color="deep-purple"
+        height="10"
+        indeterminate
+      ></v-progress-linear>
+    </template>
+
+    <v-img
+      height="200"
+      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+    ></v-img>
+
+    <v-card-title>Cafe Badilico</v-card-title>
+
+    <v-card-text>
+      <v-row align="center" class="mx-0">
+        <v-rating
+          :value="4.5"
+          color="amber"
+          dense
+          half-increments
+          readonly
+          size="14"
+        ></v-rating>
+
+        <div class="grey--text ms-4">4.5 (413)</div>
+      </v-row>
+
+      <div class="my-4 text-subtitle-1">$ • Italian, Cafe</div>
+
+      <div>
+        Small plates, salads & sandwiches - an intimate setting with 12 indoor
+        seats plus patio seating.
+      </div>
+    </v-card-text>
+
+    <v-divider class="mx-4"></v-divider>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "pcard",
+  data: () => ({}),
+};
+</script>
---
+++ kandyness/src/views/paginaLojas.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <v-sheet
+      class="text--white"
+      color="purple"
+      elevation="1"
+      height="100"
+      width="100%"
+      style="display: flex; align-items: center"
+      ><h1 style="font-size: 50px; color: white; margin-left: 16px">
+        Exemplo de loja
+      </h1></v-sheet
+    >
+    <div style="margin-left: 16px; margin-top: 50px">
+      <span style="color: #9c27b0; font-size: 25px; font-weight: 350">
+        Produtos
+      </span>
+    </div>
+    <v-divider class="purple lighten-4"></v-divider>
+    <div
+      style="
+        margin-left: 16px;
+        display: grid;
+        grid-template-columns: repeat(5, 250px);
+        grid-column-gap: 50px;
+        grid-row-gap: 0px;
+        justify-content: center;
+      "
+    >
+      <pcard></pcard>
+      <pcard></pcard>
+      <pcard></pcard>
+      <pcard></pcard>
+      <pcard></pcard>
+      <pcard></pcard>
+      <pcard></pcard>
+      <pcard></pcard>
+      <pcard></pcard>
+      <pcard></pcard>
+      <pcard></pcard>
+      <pcard></pcard>
+    </div>
+  </div>
+</template>
+
+<script>
+import pcard from "@/components/pcard";
+export default {
+  name: "myView",
+  components: { pcard },
+};
+</script>
