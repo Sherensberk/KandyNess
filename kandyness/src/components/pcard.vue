f02c6ad487c4d60a8aedaf35cba8ca98dd251e2d
Joao Vitor Siqueira <jvitorsiqueira99@gmail.com>
2023-05-08 08:58:34 -0300

    edição adicionada

--- kandyness/src/components/pcard.vue
+++ kandyness/src/components/pcard.vue
@@ -15,7 +15,7 @@
         <v-icon dark> mdi-delete </v-icon>
       </v-btn>
       <v-btn class="mt-n4 mr-0" fab dark x-small color="blue" absolute right>
-        <v-icon dark> mdi-pencil </v-icon>
+        <v-icon dark v-on:click="openModal"> mdi-pencil </v-icon>
       </v-btn>
     </v-img>
 
@@ -34,7 +34,42 @@
 
         <div class="grey--text ms-4">4.5 (413)</div>
       </v-row>
+      <!--inicio do modal-->
+      <dialog :id="id">
+        <v-container>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                :id="nameInput"
+                label="Nome do produto"
+              ></v-text-field>
+            </v-col>
 
+            <v-col cols="12">
+              <v-text-field
+                label="Categoria do produto"
+                :id="catInput"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-text-field
+                label="Descrição do produto*"
+                :id="descInput"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-text-field :id="imgInput" label="Imagem do produto">
+              </v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+        <div class="text-center">
+          <v-btn v-on:click="editCard" color="blue" elevation="2" width="120"
+            ><span style="color: white">Salvar</span></v-btn
+          >
+        </div>
+      </dialog>
+      <!--Fim do modal-->
       <div class="my-4 text-subtitle-1">{{ category }}</div>
       <div>
         <span> {{ desc }} </span>
@@ -50,11 +85,23 @@ export default {
     desc: String,
     picture: String,
     category: String,
+    id: String,
+    nameInput: String,
+    catInput: String,
+    descInput: String,
+    imgInput: String,
   },
   data: () => ({}),
   methods: {
     deleteCard: function () {
       this.$emit("delete-card", this.index);
+    },
+    editCard: function () {
+      this.$emit("edit-card", this.index);
+    },
+    openModal: function () {
+      let modal = document.getElementById(this.id);
+      modal.showModal();
     },
   },
 };
